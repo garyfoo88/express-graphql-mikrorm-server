1d1acc904079b1dc0178d43b20debc94dc997f63
garyfoo88 <reid9000200020002000@gmail.com>
2021-06-13 00:50:10 +0800

    added sessions and redis connection

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,44 @@ import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
 
 const main = async () => {
   const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+        disableTTL: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+        httpOnly: true,
+        sameSite: "lax", //csrf
+        secure: __prod__, //cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: "wehfoewhoufhewfew",
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
 
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -6,10 +6,10 @@ import {
   Field,
   InputType,
   Mutation,
+  ObjectType,
   Resolver,
 } from "type-graphql";
 import argon2 from "argon2";
-
 
 @InputType()
 class UsernamePasswordInput {
@@ -19,16 +19,113 @@ class UsernamePasswordInput {
   password: string;
 }
 
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
+
+  
+  @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
     @Ctx() ctx: MyContext
-  ) {
-    const hashedPassword = await argon2.hash(options.password)
-    const user = ctx.em.create(User, { username: options.username, password: hashedPassword });
-    await ctx.em.persistAndFlush(user);
-    return user;
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "length must be greater than 2",
+          },
+        ],
+      };
+    }
+
+    if (options.password.length <= 3) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "length must be greater than 3",
+          },
+        ],
+      };
+    }
+
+    const hashedPassword = await argon2.hash(options.password);
+    const user = ctx.em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+    try {
+      await ctx.em.persistAndFlush(user);
+    } catch (err) {
+      if (err.code === "23505" || err.detail.includes("already exists")) {
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "username already exists",
+            },
+          ],
+        };
+      }
+    }
+
+    return {
+      user,
+    };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() ctx: MyContext
+  ): Promise<UserResponse> {
+    const user = await ctx.em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "that username does not exist",
+          },
+        ],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, options.password);
+
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "incorrect password",
+          },
+        ],
+      };
+    }
+
+    ctx.req.session!.userId = user.id;
+
+    return {
+      user,
+    };
   }
 }
